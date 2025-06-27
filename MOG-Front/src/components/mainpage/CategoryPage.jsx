aaf222640b7d9b523e8cf216cafefdcb162f6a9a
kimbongkum <qkdlzld80@naver.com>
2025-06-27 18:21:22 +0900

    feat:루틴 생성 페이지 수정
    루틴 생성 로직 구성

--- MOG-Front/src/components/mainpage/CategoryPage.jsx
+++ MOG-Front/src/components/mainpage/CategoryPage.jsx
@@ -1,25 +1,32 @@
 import { Link, useNavigate } from "react-router-dom";
 import styles from "../../assets/bootstrap/css/mainpage.module.css";
 import "../../assets/bootstrap/css/bootstrap.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function CategoryPage(){
     const navigate = useNavigate();
-    const [routineContainer,setRoutineContainer] = useState(0);
+    const [routineContainer,setRoutineContainer] = useState('');
+    const makeRoutineContainer = useRef(null);
+    const makeRoutineBox =()=>{
+        const li = document.createElement('button'); 
+        li.innerHTML = `<h1>새로운 루틴 1</h1>`;
+       document.querySelector(`.container.mt-0.p-0`).appendChild(li);
+    }
     useEffect(()=>{
-        
+        console.log(document.querySelector(`.container.mt-0.p-0`));
     },[])
     return<>
         <div className={"container mt-5 p-3"}></div>
-        <button className={`btn btn-lg btn-primary`} type="button" onClick={()=>navigate(-1)}>뒤로가기</button>
-        <div className={`${styles.mainpage} container mt-0 p-0`}>
-                    <form className="d-flex">
-                        <input className="form-control me-sm-2" type="search" placeholder="Search"/>
-                        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                <div className={"container mt-0 p-0"}>
-                    <h1>루틴 생성 페이지</h1>
-                </div>
+            <button className={`btn btn-lg btn-primary`} type="button" onClick={()=>navigate(-1)}>뒤로가기</button>
+            
+            <form className="d-flex">
+                <input className="form-control me-sm-2" type="search" placeholder="Search"/>
+                <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <div ref={makeRoutineContainer} className={`${styles.mainpage} container mt-0 p-0`}>
+                    <div className={"container mt-0 p-0"}>
+                        <h1>루틴 생성 페이지</h1>
+                    </div>
             <div className={`${styles.dummyContainers} p-5 mt-4`}></div>
             <footer className={`${styles.flexButton}`}>
                 <button className={`${styles.buttonSize} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/routine")}>운동 추가</button>
