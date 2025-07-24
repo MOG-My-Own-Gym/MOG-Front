5698e44279fd29409f98273c0ae871e7d890d15f
kimbongkum <qkdlzld80@naver.com>
2025-07-24 19:10:33 +0900

    feat:루틴 실행 부터 종료까지 완료
    axios를 이용한 모든 데이터 형식 작성 완료

--- MOG-Front/src/pages/mainpage/RoutinePage.jsx
+++ MOG-Front/src/pages/mainpage/RoutinePage.jsx
@@ -5,49 +5,65 @@ import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { URL } from "../../config/constants";
 
-export default function RoutinePage({useDataRoutine}){
+export default function RoutinePage({
+        useDataRoutine,
+        fetchData,
+        detailData,
+        startRrcodResultData,
+        setIsOpen,
+        setMakeRoutine,
+        initMakeRoutine,
+        setCheckPageWhenRunningPage,
+        setRoutineId,
+        currentRrcodingRoutineId
+    }){
     const navigate = useNavigate();
-    const [initMakeRoutine, setMakeRoutine] = useState([]);
     const makeRoutineBoxRef = useRef();
     const {search } = useLocation();
     const params = search.slice(-1);
-    const [deleteEx,setDeleteEx] = useState();
+    const [initDetailDEx,setDetailEx] = useState();
 
     const routineDetailButton=(e)=>{
         e.stopPropagation();
-        console.log('initMakeRoutine:',initMakeRoutine);
-
+        const checkId = e.target.children[1]===undefined?e.target.id:e.target.children[1].id;
+        setCheckPageWhenRunningPage(true);
+        navigate(`/data/runningroutine?routineId=${params}&DetailId=${checkId}`,{state:initMakeRoutine});
+    }
+    const startRoutine=()=>{
+        if(!startRrcodResultData){
+            setCheckPageWhenRunningPage(false);
+            setIsOpen(true);
+        }
+    }
+    const loadRoutineDetail=async ()=>{
+            await axios.get(URL.ROUNTINE)
+            .then(res=> setMakeRoutine(res.data[params-1].state))
+            await axios.get(URL.ROUTINEDETAIL)
+            .then(res=> setDetailEx(res.data[params-1].state))
     }
     const deleteRoutine=async (e)=>{
          e.stopPropagation();
-        //const lastS = e.target.id.charAt(e.target.id.length - 1);
-        //console.log('lastS:',lastS);
-        //await axios.put(`${URL.ROUNTINE}/${location.state}/`,{})
-        //            .then()
-        //await axios.delete(`${URL.ROUNTINE}/${lastS}`)
-        //fetchData();
-        console.log('루틴 페이지 location.state2:',params);
-        console.log('운동 삭제 버튼 이벤트 ID:',e.target.id);
         const deleteSaveData = initMakeRoutine.filter(item=>(item.set_id!==e.target.id));
-        console.log('initMakeRoutine.set_id:',deleteSaveData);
-        console.log('루틴 페이지 useDataRoutine:',useDataRoutine);
+        const deleteDetailData = initDetailDEx.filter(item=>(item.id!==e.target.id))
+       
         await axios.put(`${URL.ROUNTINE}/${params}/`,{id:String(useDataRoutine[params-1].id),name:useDataRoutine[params-1].name,state:[...deleteSaveData]})
-                    .then(res=>console.log('운동 삭제 결과 값:%o',res));
+        await axios.put(`${URL.ROUTINEDETAIL}/${params}`,{id:String(useDataRoutine[params-1].id),state:[...deleteDetailData]})
         setMakeRoutine(deleteSaveData);
-        
+        setDetailEx(deleteDetailData);
+    }
+    const endRoutine=()=>{
+        setIsOpen(true);
     }
 
     useEffect(()=>{
-        //setMakeRoutine([...location.state]);
-        axios.get(URL.ROUNTINE)
-            .then(res=> setMakeRoutine(res.data[params-1].state))
-            //.then(res=> console.log(res.data[location.state-1].state))
-        console.log('initMakeRoutine:',initMakeRoutine);
-        console.log('루틴 페이지 location.state1:',params);
+        loadRoutineDetail();
+        setRoutineId(params);
+        fetchData();
+        detailData();
     },[])
     return<>
         <div className={"container mt-5 p-3"}></div>
-        <button className={`btn btn-lg btn-primary`} type="button" onClick={()=>navigate('/data/')}>뒤로가기</button>
+        <button className={`btn btn-lg btn-danger`} type="button" onClick={()=>navigate('/data/')}>뒤로가기</button>
         <div className={`${styles.mainpage} container mt-0 p-0`}>
                 <div ref={makeRoutineBoxRef} className={"container mt-0 p-0"}>
                     {
@@ -63,10 +79,14 @@ export default function RoutinePage({useDataRoutine}){
                     }             
                 </div>
             <div className={`${styles.dummyContainers} p-5 mt-4`}></div>
+            {startRrcodResultData===false||currentRrcodingRoutineId===params?
             <footer className={`${styles.flexButton}`}>
-                <button className={`${styles.buttonSize} btn btn-lg btn-primary`} type="button" onClick={(e)=>{navigate("/data/runningroutine");e.target.value=='운동 완료'?e.target.value='운동 시작':e.target.value='운동 완료';}}>운동 시작</button>
+                <button className={`${styles.buttonSize} btn btn-lg ${startRrcodResultData?"btn-success":"btn-primary"} `} type="button" id="1" onClick={e=>startRrcodResultData?endRoutine():startRoutine()}>{startRrcodResultData?"운동 완료":"운동 시작"}</button>
                 <button className={`${styles.buttonSize} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/data/select",{state:params})}>운동 추가</button>
             </footer>
+            :
+            <footer></footer>
+            }
         </div>
 
     </>
