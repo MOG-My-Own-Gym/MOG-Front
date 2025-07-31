b6c23716f83c0164e35db0d96ab049b0b33c0652
kimbongkum <qkdlzld80@naver.com>
2025-07-31 10:09:28 +0900

    refactor:시간 값 및 주소 수정

--- MOG-Front/src/config/constants.js
+++ MOG-Front/src/config/constants.js
@@ -2,5 +2,5 @@ export const URL = {
   ROUNTINE: 'https://mogapi.kro.kr/json/routine',
   ROUTINEDETAIL: 'https://mogapi.kro.kr/json/routine_detail',
   ROUTINERESULT: 'https://mogapi.kro.kr/json/routine_result',
-  REALDATA: 'https://mogapi.kro.kr/api/v1/routine/1/result',
+  REALDATA: 'https://mogapi.kro.kr/api/v1/routine',
 };
