c9bc8a82a5618fd4b0ec62d4d16d4e5d8b3681db
HSYPRO <august2013@naver.com>
2025-08-12 19:09:20 +0900

    feat : 헤더추가
    1. 헤더추가

--- MOG-Front/src/components/Header/Header.jsx
+++ MOG-Front/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import ToastContext from '../../context/ToastContext';
 export default function Header() {
   return (
     <header className="masthead" style={{ backgroundImage: `url(/img/hell.jpg)` }}>
+      <></>
       <div className="container">
         <div className="masthead-subheading">당신의 헬스 트레이너</div>
         <div className="masthead-heading text-uppercase">만나서 반갑습니다.</div>
